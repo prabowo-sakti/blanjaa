9b37660ca5ec45dc16bb91144f481fa345c25f79
prabowo-sakti <prabowosakti2023@gmail.com>
2025-01-12 20:03:18 +0700

    penambahan navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+      <p>Blanjaa merupakan e-commerce khusus produk virtual</p>  
+        </>
+    
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
-import './globals.css'
+import Navbar from "components/Navbar";
+import "./globals.css";
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="id">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+        <footer>
+          <p>&copy; 2025 E-Commerce Anda. Semua hak cipta dilindungi.</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
